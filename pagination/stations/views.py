226316b1898b1711912741ba23cf3eb31d08a5c9
Ilya NSK <Ilyagl.nsk@yandex.ru>
2024-08-06 22:43:50 +0700

    Add pagination

--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -9,9 +11,16 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
+    with open('C:/Users/Desktop/dz_django/dj-homeworks/1.2-requests-templates/pagination/data-398-2018-08-30.csv', 'r', encoding='utf-8') as file:
+        bus_stations_ = []
+        for row in csv.DictReader(file):
+            bus_stations_.append(row)
+        page_number = request.GET.get('page')
+        paginator = Paginator(bus_stations_, 10)
+        page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+
     }
     return render(request, 'stations/index.html', context)
